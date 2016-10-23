56d7360a09cac2ca2df2811899acb942e33a2bde
Florent MARCAILLE <f.marcaille@yahoo.fr>
2016-10-23 19:18:41 +0200

    To commit for Alex branch change

--- src/graphtest/syntaxor/PriorityTools.java
+++ src/graphtest/syntaxor/PriorityTools.java
@@ -9,6 +9,7 @@ import graphtest.parsed.TOK_OPERATOR_PLUS;
 import graphtest.parsed.TOK_PAR_CLOSE;
 import graphtest.parsed.TOK_PAR_OPEN;
 import graphtest.parsed.TOK_VARIABLE;
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 /**
@@ -27,6 +28,11 @@ public class PriorityTools {
         priorityArray = setPriorityParenthesis(lexicalArray);
         
         int coef = coefficientCalculator(priorityArray);
+        BigInteger prio = new BigInteger("0");
+        
+        for(int i=0;i<priorityArray.size();i++){
+            if(priorityArray.get(i))
+        }
         
         return priorityArray;
     }
